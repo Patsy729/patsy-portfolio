d7e729703c18541ecc43843821f818aee1d7f7ad
Patsy729 <patsy.29school@gmail.com>
2025-04-16 11:21:49 +0800

    freelance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import AppRoutes from './routes/AppRoutes'
 import Navbar from './components/Navbar'
 import SolarSystem from './components/SolarSystem'
@@ -6,24 +5,7 @@ import ButterflyDesign from './components/ButterflyDesign'
 import './App.css'
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false)
-    }, 2000)
-
-    return () => clearTimeout(timer)
-  }, [])
-
-  if (isLoading) {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-white">
-        <div className="animate-pulse text-4xl font-bold text-[#ff00ff]">Loading...</div>
-      </div>
-    )
-  }
-
+  // Removed loading state for testing deployment white screen issue
   return (
     <div className="relative bg-[#fefefe]">
       <Navbar />
@@ -36,4 +18,4 @@ function App() {
   )
 }
 
-export default App 
+export default App
